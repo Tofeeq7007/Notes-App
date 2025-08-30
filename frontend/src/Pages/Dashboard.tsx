30866a4bb28945fe64f938a23baeeb67775998ff
Tofeeq7007 <tofeeqraja9100@gmail.com>
2025-08-31 01:36:58 +0530

    Dashboard UI Complete

--- frontend/src/Pages/Dashboard.tsx
+++ frontend/src/Pages/Dashboard.tsx
@@ -1,7 +1,31 @@
+import { Notes } from "../component/Notes"
+import { Button } from "../component/ui/button"
+import { UserInfo } from "../component/UserInfo"
+
 export const Dashboard = () => {
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className=" flex flex-col items-center justify-center">
+            <div className="flex justify-between mt-4 gap-2 items-center w-96">
+                <div className="flex justify-between gap-4 items-center">
+                    <div className="">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-8 text-[#367AFF]">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                        </svg>                    
+                    </div>
+                    <div className="font-inter font-medium text-xl leading-[110%] tracking-[-4%]">Dashboard</div>
+                </div>
+                <button className="underline text-sm leading-[150%] font-inter text-[#367AFF] font-semibold">Sign out</button>
+            </div>
+            {/*  */}
+            <UserInfo/>
+            <Button text={"Create Note"} size="md"/>
+            <div className="w-98">
+                <div className="flex  font-inter mt-9 font-medium text-xl leading-[110%] tracking-[-4%]">Notes</div>
+                <Notes text={"Notes 1"} id={"122"}/>
+                <Notes text={"Notes 2"} id={"122"}/>
+            </div>
+
+            {/*  */}
         </div>
     )
 }
---
+++ frontend/src/component/UserInfo.tsx
@@ -0,0 +1,10 @@
+export const UserInfo = ()=>{
+
+
+    return  <div className="w-98 mt-16 mb-9">
+                <div className="border-1 p-4 gap-2.5 border-[#D9D9D9] rounded-md shadow-[0px_2px_6px_0px_#00000096]">
+                    <div className="font-bold text-[22px] leading-[250%] text-[#232323] font-inter">Welcome, Jonas Kahnwald !</div>
+                    <div className="font-normal font-inter leading-[250%] " >Email: jonas.kahnwald@company.com</div>
+                </div>
+            </div>
+}
